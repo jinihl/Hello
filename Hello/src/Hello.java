4b77bbc66f2f166e46e47baa82e9495aeca506c1
Jinihl Kim Developer <jinihl@gmail.com>
2011-09-09 14:14:42 +0900

    Hello java add

---
+++ Hello/src/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author 유원미
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1st Hello...");
+		System.out.println("2nd Hello...");
+	}
+
+}
